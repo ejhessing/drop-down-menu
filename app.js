0da38a0edb66e5aabcaf6a17f49d2b839d650dfc
Erwin <ejhessing@gmail.com>
2016-05-09 16:09:03 +1200

    Added Select drop down menu

--- app.js
+++ app.js
@@ -11,3 +11,19 @@
 //Modify CSS to hide links on small width and show button and select 
 	// Also hides select and button on larger width and shows links
 // Deal with selected options depending on current page
+
+var $select = $("<select></select>");
+$(".menu").append($select);
+
+$(".menu a").each(function(){
+	var $anchor = $(this);
+	var $option = $("<option></option>");
+
+	$option.val($anchor.attr("href"));
+
+	var anchorText = $anchor.text();
+	$option.text(anchorText);
+
+	$select.append($option);
+
+});
