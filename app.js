a5bfbf45b198f704f0b15c159a7afd6f291b7769
Erwin <ejhessing@gmail.com>
2016-05-09 15:56:24 +1200

    Sudo code for app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// Create a drop down menu for mobile devices
+
+// Create a select and append to menu
+// Cycle over menu links
+	// Create an option
+	// Option's Value is the href
+	// Options Text is the text of link
+	// Append option to select
+//Create a button 
+	// bind click to go to select's location
+//Modify CSS to hide links on small width and show button and select 
+	// Also hides select and button on larger width and shows links
+// Deal with selected options depending on current page
